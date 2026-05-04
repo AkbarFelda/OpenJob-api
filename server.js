d1e846b66e6258db9df249db174e40e3c347f479
AkbarFelda <akbarfelda65@gmail.com>
2026-05-04 12:53:37 +0700

    setup: [AF] setup project

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'OpenJob API v1 running!' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
